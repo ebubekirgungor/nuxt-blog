feaf891af9744cdb2d9006fa9d353de91d5c7141
ebubekirgungor <ebubekirgungor2003@gmail.com>
2023-09-15 18:17:38 +0300

    Added users page in admin panel

--- pages/admin/users.vue
+++ pages/admin/users.vue
@@ -6,7 +6,38 @@ definePageMeta({
 useHead({
   title: "Users",
 });
+
+const { data: users } = await useFetch("/api/users");
 </script>
 <template>
-  <h1>users</h1>
+  <div class="relative overflow-x-auto">
+    <table
+      class="w-[calc(100vw-19rem)] fixed mr-15 text-left border border-collapse border-0 overflow-hidden bg-white rounded-xl shadow-xl text-gray-500"
+    >
+      <thead class="select-none text-gray-700 bg-gray-50">
+        <tr>
+          <th scope="col" class="px-6 py-3">Username</th>
+          <th scope="col" class="px-6 py-3">Email</th>
+          <th scope="col" class="px-6 py-3">Name</th>
+          <th scope="col" class="px-6 py-3 text-right">Posts</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" class="bg-white border-b">
+          <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+            {{ user!.username }}
+          </td>
+          <td class="px-6 py-4">
+            {{ user!.email }}
+          </td>
+          <td class="px-6 py-4">
+            {{ user!.name }}
+          </td>
+          <td class="px-6 py-4 text-right">
+            {{ user!.posts }}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
--- server/api/users/index.get.ts
+++ server/api/users/index.get.ts
@@ -1,13 +1,25 @@
 import users from "../../models/user";
+import posts from "../../models/post";
 export default defineEventHandler(async (event) => {
   console.log("GET /api/users");
   try {
     console.log("Find users");
     const usersData = await users.find();
-    return usersData.map((user) => ({
-      id: user._id,
-      username: user.username,
-    }));
+
+    const userDataWithPosts = await Promise.all(
+      usersData.map(async (user) => {
+        const userPosts = await posts.find({ author: user.username });
+        return {
+          id: user._id,
+          username: user.username,
+          email: user.email,
+          name: user.name,
+          posts: userPosts.length,
+        };
+      })
+    );
+
+    return userDataWithPosts;
   } catch (err) {
     console.dir(err);
     event.res.statusCode = 500;
