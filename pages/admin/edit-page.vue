f7117fbd6dde0921781bbab474099446032b74b4
Ebubekir Güngör <115069398+ebubekirgungor@users.noreply.github.com>
2023-11-30 21:25:29 +0300

    Update edit-page.vue

--- pages/admin/edit-page.vue
+++ pages/admin/edit-page.vue
@@ -29,16 +29,13 @@ definePageMeta({
 useHead({
   title: t("edit_page"),
 });
-const input = ref(
-  "transition duration-200 ease-in-out ml-20 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:outline-none focus:ring-sky-400 focus:border-sky-400 block w-[250px] h-9 p-2.5"
-);
-const textarea = ref(
-  "transition duration-200 ease-in-out ml-20 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:outline-none focus:ring-sky-400 focus:border-sky-400 block w-[250px] p-2.5"
-);
-const label = ref("select-none mt-1");
-const button = ref(
-  "transition duration-200 ease-in-out px-5 py-2 mt-7 bg-sky-500 hover:bg-sky-700 rounded-md text-white select-none"
-);
+const input = 
+  "transition duration-200 ease-in-out ml-20 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:outline-none focus:ring-sky-400 focus:border-sky-400 block w-[250px] h-9 p-2.5";
+const textarea = 
+  "transition duration-200 ease-in-out ml-20 bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:outline-none focus:ring-sky-400 focus:border-sky-400 block w-[250px] p-2.5";
+const label = "select-none mt-1";
+const button = 
+  "transition duration-200 ease-in-out px-5 py-2 mt-7 bg-sky-500 hover:bg-sky-700 rounded-md text-white select-none";
 
 const updatedPage = ref({
   name: page.value.name,
