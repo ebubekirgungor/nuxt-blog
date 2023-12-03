40272fa09e16e0c10491cb694b1d37ec84c4d407
Ebubekir Güngör <115069398+ebubekirgungor@users.noreply.github.com>
2023-12-03 20:38:49 +0300

    Update index.vue

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -16,9 +16,8 @@ interface Count {
 
 const { data: count } = await useFetch<Count | any>("/api/posts?type=count");
 
-const box = ref(
-  "scale-75 md:scale-100 flex flex-col justify-between text-2xl bg-white min-w-[300px] min-h-[175px] rounded-xl shadow-xl p-8 select-none"
-);
+const box = 
+  "scale-75 md:scale-100 flex flex-col justify-between text-2xl bg-white min-w-[300px] min-h-[175px] rounded-xl shadow-xl p-8 select-none";
 </script>
 <template>
   <div :class="box">
